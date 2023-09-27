e2fd478da7b967c31d4436aaa78c1e2184928688
Wollace Bomfim <57396168+wollacebomfim@users.noreply.github.com>
2023-09-27 15:12:23 -0300

    Update conexao.php
    Alterado

--- conexao/conexao.php
+++ conexao/conexao.php
@@ -1,7 +1,7 @@
 <?php
-define('HOST', 'queirozcavalcanti.adv.br');
-define('USUARIO', 'qca');
-define('SENHA', 'rfngy7vmMs3ZB6b');
-define('DB', 'qca_relatorios');
+define('HOST', '');
+define('USUARIO', '');
+define('SENHA', '');
+define('DB', '');
 
 $conexao = mysqli_connect(HOST, USUARIO, SENHA, DB) or die ('Não foi possível conectar');
